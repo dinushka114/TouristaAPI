a60a246d685659e29d6ccaa1f05662b0004906cb
dinushka114 <dinushkapiyumal678@gmail.com>
2023-05-10 13:32:26 +0530

    get my trips donw

--- controllers/plan-trip/plan-trip.js
+++ controllers/plan-trip/plan-trip.js
@@ -1,4 +1,6 @@
 const Trip = require("../../database/models/Trip");
+const jwt = require("jsonwebtoken");
+require("dotenv").config()
 
 const planTrip = async (req, res) => {
     try {
@@ -25,4 +27,26 @@ const planTrip = async (req, res) => {
 
 }
 
-module.exports = planTrip;
+const getMyTrips = async (req, res) => {
+    try {
+        const authHeader = req.headers["authorization"];
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.SECRET,async (err, decoded) => {
+            if (err) return res.sendStatus(403); //invalid token
+            let user = decoded.email;
+
+            const myTrips = await Trip.find({ user });
+            return res.status(200).json(myTrips);
+      
+        });
+
+     
+
+    } catch (err){
+        return res.status(500).json({
+            message: `${err.message}`
+        });
+    }
+}
+
+module.exports = { planTrip, getMyTrips };
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,4 +1,4 @@
-const planTrip = require("../../controllers/plan-trip/plan-trip");
+const {planTrip , getMyTrips} = require("../../controllers/plan-trip/plan-trip");
 const { userAuth, checkRole } = require("../../middlewares");
 
 const router = require("express").Router();
@@ -6,5 +6,10 @@ const router = require("express").Router();
 router.post('/plan-trip' , userAuth , checkRole(["User"]) , async(req,res)=>{
     await planTrip(req,res)
 })
+
+router.get('/my-trips' , userAuth , async(req,res)=>{
+    await getMyTrips(req , res);
+})
+
 
 module.exports = router;
